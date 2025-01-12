3e74701dcaf3f249f569c0d033979bbddd00b6b8
Patrik Drevestedt <patrik.drevestedt@me.com>
2025-01-12 03:34:28 +0100

    add booking view and configure URLs for booking app

--- meathook/urls.py
+++ meathook/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from booking.views import booking
 
 urlpatterns = [
+    path('', booking, name='home'),
+    path('booking/', booking, name='booking'),
     path('admin/', admin.site.urls),
 ]
