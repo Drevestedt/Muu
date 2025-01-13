9cf7600d3701a8310dd62a30ad5b0d20712987d9
Patrik Drevestedt <patrik.drevestedt@me.com>
2025-01-13 10:17:07 +0100

    add booking model, form, and views for table reservation

--- booking/views.py
+++ booking/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Booking
 
 # Create your views here.
-def booking(request):
-  return HttpResponse("Booking Page")
+def book_table(request):
+  if request.method == 'POST':
+    form = Booking(request.POST)
+    if form.is_valid():
+      form.save()
+      return render(request, 'booking/success.html')
+  else:
+    form = Booking()
+  return render(request, 'booking/book_table.html', {'form': form})
+
+#def booking(request):
+  #return HttpResponse("Booking Page")
