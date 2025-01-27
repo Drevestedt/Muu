d69b4aeff89045225a6ad221ee1c715d3d932693
Patrik Drevestedt <patrik.drevestedt@me.com>
2025-01-27 23:00:42 +0100

    Starting over some

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn meat_hook.wsgi
--- booking/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-  path('book_table/', views.book_table, name='book_table'),
-  path('booking_confirmation/', views.booking_confirmation, name='booking_confirmation'),
-]
