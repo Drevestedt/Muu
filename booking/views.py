0429455962c42215b315eb2d40968d00494e9419
Patrik Drevestedt <patrik.drevestedt@me.com>
2025-01-28 19:02:46 +0100

    Add base and home templates

--- booking/views.py
+++ booking/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
-def test_view(request):
-    return HttpResponse("Test view")
+def base(response):
+  return render(response, "base.html", {})
+
+def home(response):
+  return render(response, "home.html", {})
