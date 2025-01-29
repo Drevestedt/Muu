78dfe99296eefa6f3d4925d7c609c2a23bc36690
Patrik Drevestedt <patrik.drevestedt@me.com>
2025-01-29 07:41:38 +0100

    Configure database settings and set DEBUG to False for production readiness

--- meat_hook/settings.py
+++ meat_hook/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os 
 from pathlib import Path
-
+import dj_database_url
+if os.path.isfile("env.py"):
+  import env 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -30,7 +32,7 @@ STATICFILES_DIRS = [
 SECRET_KEY = 'django-insecure-fsv_)+tzdtyedn_^8g^-_zd5n%lxam!td0hg(cgtt$3d5vcu5a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True 
+DEBUG = False  
 
 ALLOWED_HOSTS = []
 
@@ -81,13 +83,16 @@ WSGI_APPLICATION = 'meat_hook.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
 
+DATABASES = {
+  "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
