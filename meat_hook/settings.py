a9c20270ee240f4dfa00386aeb4998985c1fd2c8
Patrik Drevestedt <patrik.drevestedt@me.com>
2025-01-29 14:01:42 +0100

    Fix static file settings and ensure correct STATIC_URL configuration

--- meat_hook/settings.py
+++ meat_hook/settings.py
@@ -19,12 +19,6 @@ if os.path.isfile("env.py"):
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Static files (CSS, JS, Images)
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -32,7 +26,7 @@ STATICFILES_DIRS = [
 SECRET_KEY = 'django-insecure-fsv_)+tzdtyedn_^8g^-_zd5n%lxam!td0hg(cgtt$3d5vcu5a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False  
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", ".herokuapp.com"]
 
@@ -128,8 +122,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
