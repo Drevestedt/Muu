73d0f80234603c11ef17d34e9e75d25f6821fbdc
Patrik Drevestedt <patrik.drevestedt@me.com>
2025-01-30 07:55:02 +0100

    Add WhiteNoise for static file handling and configure STATIC_ROOT

--- meat_hook/wsgi.py
+++ meat_hook/wsgi.py
@@ -10,7 +10,12 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meat_hook.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'static'))
