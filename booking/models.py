fc1b3b9b08fddc71d5ef27ca9f83097a6e239128
Patrik Drevestedt <patrik.drevestedt@me.com>
2025-02-03 08:46:29 +0100

    Rename 'num_of_guests' to 'party_size' in Booking model and update migration file

--- booking/models.py
+++ booking/models.py
@@ -7,7 +7,8 @@ class Bookings(models.Model):
     phone = models.CharField(max_length=15)
     date = models.DateField()
     time = models.TimeField()
-    num_of_guests = models.IntegerField()
+    party_size = models.IntegerField()
 
     def __str__(self):
-        return f"{self.name} - {self.email} - {self.phone} - {self.date} - {self.time} - {self.num_of_guests}"
+        return f"{self.name} - {self.email} - {self.phone} - {self.date} - {self.time} - {self.party_size}"
+    
