995c2b8cadaa2d389387c7fe7237b2d4d2c03243
Patrik Drevestedt <patrik.drevestedt@me.com>
2025-02-10 10:52:55 +0100

    Implement responsive navbar with toggle functionality and update styles

--- staticfiles/scripts/base.js
+++ staticfiles/scripts/base.js
@@ -2,3 +2,28 @@ function toggleMenu() {
   const menu = document.querySelector(".menu");
   menu.classList.toggle("active");
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const menuIcon = document.querySelector(".menu-icon");
+  const menu = document.querySelector(".menu");
+
+  // Toggle the menu when the menu icon is clicked
+  menuIcon.addEventListener("click", () => {
+    menu.classList.toggle("active");
+  })
+
+  // Hide menu when a link is clicked
+  const menuLinks = document.querySelectorAll(".menu a");
+  menuLinks.forEach(link => {
+    link.addEventListener("click", () => {
+      menu.classList.remove("active");
+    });
+  });
+
+  // Hide menu when the user clicks outside of it
+  document.addEventListener("click", event => {
+    if (!menu.contains(event.target) && !menuIcon.contains(event.target)) {
+      menu.classList.remove("active");
+    }
+  });
+});
